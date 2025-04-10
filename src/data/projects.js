d002e2a6b88307d36b7db0aa42d262938bbda641
Alexander Tsai <alexandertsaiyihrae229@gmail.com>
2025-04-11 03:33:17 +0800

    Project updates

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,13 @@
 export const projects = [
     {
-      id: 1,
+      id: 4,
+      title: "Telegram MCP",
+      description: "Allows you to summarise unread Telegram messages and reply in your style from Claude Desktop.",
+      technologies: ["Python", "MCP", "Telegram API"],
+      link: "https://github.com/alexandertsai/mcp-telegram"
+    },
+    {
+      id: 3,
       title: "Invoice Automator",
       description: "Automating the generation and sending of invoices.",
       technologies: ["AppleScript", "Python"],
@@ -14,7 +21,7 @@ export const projects = [
       link: "https://kada.sg"
     },
     {
-      id: 3,
+      id: 1,
       title: "Portfolio Website",
       description: "The website you are viewing now was built with React and hosted on GitHub Pages!",
       technologies: ["React", "Javascript", "Git", "CSS"],
