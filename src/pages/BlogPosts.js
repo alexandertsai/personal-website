ec71f1bd3d25b91646efeb10fc5843f21747e991
Alexander Tsai <163396858+alexandertsai@users.noreply.github.com>
2025-07-20 08:41:49 +0800

    c

--- src/pages/BlogPosts.js
+++ src/pages/BlogPosts.js
@@ -9,12 +9,12 @@ import letterboxed from '../posts/letterboxed/index.js';
 import cjc from '../posts/cjc/index.js';
 import tradeweb from '../posts/tradeweb/index.js';
 import xgboost from '../posts/xgboost/index.js';
+import kada from '../posts/kada-utilities/index.js';
 
 export const blogPosts = [
   tradeweb,   // Add new posts from the top
   cjc,
   letterboxed
-
 ];
 
 function BlogPost() {
